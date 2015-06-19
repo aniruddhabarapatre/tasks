28a0d0e81b25673746b971c5b13c1ab98eb91194
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-06-18 20:51:26 -0400

    Create tasks
    Create new routes and root page
    Generate controller and model
    Perform db migration
    Write test for Tasks
    Create index page

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
     it "it display some task" do
+      @task = Task.create :task => "go to bed"
+
       visit tasks_path
       page.should have_content "go to bed"
     end
